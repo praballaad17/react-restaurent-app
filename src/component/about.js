a670ddbd09426de60ceb01a6cbf5f73207d19c5b
prabal laad <praballaad17@gmail.com>
2020-08-07 19:53:45 +0530

    fetch error handling

--- src/component/about.js
+++ src/component/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
 
@@ -29,7 +30,28 @@ const About = (props) => {
         );
     });
 
-    return(
+
+    if (props.leaders.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.leaders.errMess) {
+        return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.leaders.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else return(
         <div className="container">
             <div className="row">
                 <Breadcrumb>
