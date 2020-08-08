ad18dc30442526480ab0b326d05f34b3a2a1dc0b
prabal laad <praballaad17@gmail.com>
2020-08-08 16:33:31 +0530

    react animation 2

--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import { Control, LocalForm,Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -100,13 +101,18 @@ function RenderDish({dish}) {
     if (dish != null) {
         return(
             <div className="m-1" >
-            <Card>
-            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+                <FadeTransform
+                    in transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
            </div> 
         );
     }
@@ -122,12 +128,14 @@ function RenderComments({comments ,postComment, dishId}) {
         const list = comments.map((comment)=> {
 
             return(
-                <div>       
-                   <li key="comment.id">
-                    <h6 className="py-1">{comment.comment}</h6>
-                    <h6 className="py-2">--{comment.author},
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</h6>  
-                </li>
+                <div>  
+                    <Fade in>     
+                        <li key="comment.id">
+                            <h6 className="py-1">{comment.comment}</h6>
+                            <h6 className="py-2">--{comment.author},
+                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</h6>  
+                        </li>
+                    </Fade>
                 </div>
             )
         })
@@ -135,7 +143,9 @@ function RenderComments({comments ,postComment, dishId}) {
             <div>
                 <ul className="list-unstyled">
                     <h4>Comments</h4>
+                    <Stagger in>
                     {list}
+                    </Stagger>
                     <CommentForm className="py-2" postComment={postComment} dishId={dishId}/> 
                 </ul>
             </div>
